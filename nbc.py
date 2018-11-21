efd412164cb4d304a828d93fc2ad44f3aae15af0
Nam Vu Quang <vqnam97@gmail.com>
2018-11-21 19:00:34 +0700

    naive bayes classifier

--- nbc.py
+++ nbc.py
@@ -4,26 +4,22 @@ df = pd.read_csv('/home/nam/ML_181/data/restaurant_wait.csv')
 
 att_target = 'Wait'
 att_name_list = df.columns.tolist()
-att_name_list.remove('Wait')
-att_name_list = ['Pat', 'Price']
+att_name_list.remove(att_target)
 
 decisions_count, list_table_count = update_list_tables(data_frame=df, target_name=att_target)
 
 result = {}
 
-X = df.iloc[11:]
-
+# X = df.iloc[11:]
+X11 = df.iloc[11:].to_dict('records')[0]
 for each_class in decisions_count.keys():
     s = 0
     for each_attribute_name in att_name_list:
-        s+=1
-        # print(list_table_count[each_attribute_name].at[X[each_attribute_name], each_class])
-        print(list_table_count[each_attribute_name].at[X[each_attribute_name], each_class])
-        # s += np.log(list_table_count[each_attribute_name].at[\
-        #                 X[each_attribute_name], each_class])
-        #
-        # s -= np.log(np.sum(list_table_count[each_attribute_name].iloc[:each_class]))
+        s += np.log(list_table_count[each_attribute_name].at[\
+                        X11[each_attribute_name], each_class])
+
+        s -= np.log(np.sum(list_table_count[each_attribute_name][each_class].tolist()))
 
     result[each_class] = np.log(decisions_count[each_class]) + s
 
-print(result)
+print(max(result))
